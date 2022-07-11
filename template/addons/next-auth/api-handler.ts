9024ceb12920a13f972f5f10b7e384e1e069befa
Cardano G.O.A.T <Jrkbender@gmail.com>
2022-07-11 00:14:54 -0400

    feat: templating files for prisma nextjs auth tailwind trpc and base react project

---
+++ template/addons/next-auth/api-handler.ts
@@ -0,0 +1,30 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Enter your name",
+        },
+      },
+      async authorize(credentials, _req) {
+        const user = { id: 1, name: credentials?.name ?? "J Smith" };
+        return user;
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
